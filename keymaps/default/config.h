1f0c261c0471981cbe61be3fc7b460886233d6ec
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-22 11:50:45 +1000

    this works!

---
+++ keymaps/default/config.h
@@ -0,0 +1,11 @@
+// Copyright 2022 Vino Rodrigues (@vinorodrigues)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_MACRO_SIZE 512
+
+#ifdef RGB_MATRIX_ENABLE
+    #ifndef VIA_QMK_RGBLIGHT_ENABLE  // safe to remove after Q2-2022 `develop` PR
+        #define VIA_QMK_RGBLIGHT_ENABLE
+    #endif  // VIA_QMK_RGBLIGHT_ENABLE
+#endif  // RGB_MATRIX_ENABLE
--- keymaps/default/rules.mk
+++ keymaps/default/rules.mk
@@ -1,9 +1,4 @@
 # Copyright 2022 Vino Rodrigues (@vinorodrigues)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# WS2812_DRIVER = pwm
-
-# SRC += analog.c
-
-OPT = g
-OPT_DEFS += -g
+VIA_ENABLE = yes

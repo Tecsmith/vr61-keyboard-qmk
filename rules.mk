db11a9437c59238add02acf7b495995e4e210852
Vino Rodrigues <github@m.vinorodrigues.com>
2022-05-22 15:55:04 +1000

    .

--- rules.mk
+++ rules.mk
@@ -1,6 +1,8 @@
 # Copyright 2022 Vino Rodrigues (@vinorodrigues)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+LTO_ENABLE = no        // NEVER "yes" for ChibiOS mcu's
+
 # WS2812_DRIVER = pwm
 
 # SRC += analog.c

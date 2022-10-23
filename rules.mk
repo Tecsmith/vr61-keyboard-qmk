578a183f1caeaf8c25a313b6d3a5fbf3e22dd4eb
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-10-23 13:35:11 +1100

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,8 +1,4 @@
 # Copyright 2022 Vino Rodrigues (@vinorodrigues)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_DRIVER = WS2812
-# WS2812_DRIVER = bitbang
-
 DEFAULT_FOLDER = tecsmith/vr61/stm32
